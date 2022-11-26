2c0af22b9a1219f33739de2a5079883d8c2c9ff2
Ken Camarador <ken-camarador@hotmail.com>
2022-11-26 02:29:03 -0500

    deploy to heroku

--- models/gas.js
+++ models/gas.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const reviewSchema = new Schema(
   {
     content: String,
-    rating: { type: Number, min: 1, max: 5, default: 5 },
+    status: String,
   },
   {
     timestamps: true,
